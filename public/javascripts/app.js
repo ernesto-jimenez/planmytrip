9d71c77351e543a9ec7c503911e5cd21a7cf3818
sole <listas@soledadpenades.com>
2013-04-27 22:50:39 +0100

    Scroll to hide the description when loading a new result

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,7 @@ var PlanMyTripApp = function() {
 		currentSearchLocation,
 		currentResult = null,
 		searchResults = [],
+		tripId,
 		// UI
 		// Search
 		inputForm = document.getElementById('search_form'),
@@ -14,6 +15,7 @@ var PlanMyTripApp = function() {
 		buttonYes = document.querySelector('.buttons input[value=Yes]'),
 		buttonMaybe = document.querySelector('.buttons input[value=Maybe]'),
 		buttonNo = document.querySelector('.buttons input[value=No]'),
+		resultText = document.querySelector('#page_results .text'),
 		resultLandmark = document.querySelector('#page_results h1'),
 		resultCity = document.querySelector('#page_results h2'),
 		resultDescription = document.querySelector('#page_results .description')
@@ -67,7 +69,8 @@ var PlanMyTripApp = function() {
 		// TODO POST /trip/location, get response == trip id
 		// ajax(domain + '/trip/' + inputSearch.value)
 		console.log('TODO: create trip id for', tripLocation);
-		deferred.resolve('randomid');
+		tripId = 'randomid';
+		deferred.resolve(tripId);
 
 		return deferred.promise;
 	}
@@ -79,6 +82,7 @@ var PlanMyTripApp = function() {
 		ajax(domain + 'trip/' + id + '/suggestions').then(function(txt) {
 			try {
 				var results = JSON.parse(txt);
+				// TODO preload images
 				deferred.resolve(results);
 			} catch(e) {
 				deferred.reject(e);
@@ -98,16 +102,35 @@ var PlanMyTripApp = function() {
 	}
 
 	function showResult(result) {
+		var resultsPage = document.getElementById('page_results');
+
+		resultsPage.style.height = 'auto';
+
 		currentResult = result;
 		resultImage.src = result.photos[0];
 		resultLandmark.innerHTML = result.title;
 		resultCity.innerHTML = currentSearchLocation;
-		resultDescription.innerHTML = '???';
+		resultDescription.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>';
+
+		
+		// Hide the description by 'scrolling' it a little bit down
+		setTimeout(function() {
+			var textHeight = resultText.clientHeight,
+				descriptionPos = resultDescription.offsetTop;
+
+			resultsPage.style.height = (resultsPage.clientHeight + textHeight) + 'px';
+			
+			window.scrollTo(0, descriptionPos + 4);
+			
+		}, 5);
 	}
 
 	function saveResultAndShowNext(result, rating) {
 		// TODO actually save it
 		console.log('saveResultAndShowNext', rating);
+		ajax(domain + tripId + '/' + rating + '/' + result.id, {}, {method: 'post'})
+			.fail(function() {
+			});
 		showNextResult();
 	}
 
