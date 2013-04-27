54d553bc6a247047c7caaba37de2ba5b70459453
Ernesto Jiménez <me@ernesto-jimenez.com>
2013-04-27 17:14:18 +0100

    add public html

--- app.rb
+++ app.rb
@@ -15,12 +15,11 @@ set :haml, {:format => :html5} # default Haml format is :xhtml
 
 # Application routes
 get '/' do
-  haml :index, :layout => :'layouts/application'
+  File.read(File.join('public', 'index.html'))
 end
 
 post '/trip' do
-  haml :index, :layout => :'layouts/application'
-  #redirect to('/trip/whatever')
+  redirect to('/trip/whatever/edit')
 end
 
 get '/trip/:id/edit' do
@@ -35,6 +34,22 @@ get '/trip/:id/suggestions' do
     place('Times Square', [], 'bla bla bla'),
     place('Times Square', [], 'bla bla bla')
   ].to_json
+end
+
+class Trip
+  attr_reader :city, :coords
+
+  def initialize(city, coords)
+    self.city = city
+    self.coords = coords
+  end
+
+  def find(id)
+    return Trip.new('London', '51.506321,-0.127140')
+  end
+
+  def places
+  end
 end
 
 def place(name, photos, desc)
