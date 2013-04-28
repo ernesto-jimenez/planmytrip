6db4d2fc253c1ab257e9c1a0d2492d4f1b31af4c
sole <listas@soledadpenades.com>
2013-04-28 09:37:59 +0100

    image slide show

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -145,7 +145,6 @@ var PlanMyTripApp = function() {
 		resultCity.innerHTML = currentSearchLocation;
 		resultDescription.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>';
 
-		// TODO image slide show
 		if(slideShowTimeout !== null) {
 			clearTimeout(slideShowTimeout);
 		}
@@ -167,10 +166,16 @@ var PlanMyTripApp = function() {
 
 	function startSlideShow(imgElem, photos) {
 		var currentIndex = photos.indexOf(imgElem.src),
-			timeoutLength = 8000;
+			timeoutLength = 5000;
 
 		function nextPhoto() {
-			imgElem.src = photos[currentIndex];
+			imgElem.style.opacity = 0;
+			
+			setTimeout(function() {
+				imgElem.src = photos[currentIndex];
+				imgElem.style.opacity = 1;
+			}, 250);
+
 			slideShowTimeout = setTimeout(nextPhoto, timeoutLength);
 			currentIndex = ++currentIndex % photos.length;
 			return slideShowTimeout;
