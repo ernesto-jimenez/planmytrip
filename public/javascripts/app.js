4ce2ec9343fda159384f59c828679cd14b5dbfe6
sole <listas@soledadpenades.com>
2013-04-28 09:50:37 +0100

    everything tastes better with callbacks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -41,9 +41,9 @@ var PlanMyTripApp = function() {
 				.then(function(results) {
 					console.log("RESULTS!!!", results);
 
-					showPage('results');
 					searchResults = results;
-					showNextResult();
+					showPage('results', showNextResult);
+					//showNextResult();
 				})
 				.fail(function() {
 					inputSearch.classList.add('error');
@@ -143,9 +143,8 @@ var PlanMyTripApp = function() {
 		resultImage.src = result.photos[0];
 		resultLandmark.innerHTML = result.title;
 		resultCity.innerHTML = currentSearchLocation;
-    resultDescription.innerHTML = result.description || "";
-		//resultDescription.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>';
-
+		resultDescription.innerHTML = result.description || "";
+		
 		if(slideShowTimeout !== null) {
 			clearTimeout(slideShowTimeout);
 		}
@@ -168,6 +167,8 @@ var PlanMyTripApp = function() {
 	function startSlideShow(imgElem, photos) {
 		var currentIndex = photos.indexOf(imgElem.src),
 			timeoutLength = 5000;
+
+		imgElem.src = photos[currentIndex];
 
 		function nextPhoto() {
 			imgElem.style.opacity = 0;
@@ -226,9 +227,11 @@ var PlanMyTripApp = function() {
 
 	}
 
-	function showPage(name) {
+	function showPage(name, visibleCallback) {
 		var elementId = 'page_' + name,
 			elems = document.querySelectorAll('article');
+
+		visibleCallback = visibleCallback || function() {};
 
 		// current page -> fade out
 		// next page -> fade in
@@ -269,8 +272,9 @@ var PlanMyTripApp = function() {
 		}
 
 		function onNextTransitionEnd() {
-			nextPage.removeEventListener('transitionend', onTransitionEnd, false);
-			nextPage.removeEventListener('webkitTransitionEnd', onTransitionEnd, false);
+			nextPage.removeEventListener('transitionend', onNextTransitionEnd, false);
+			nextPage.removeEventListener('webkitTransitionEnd', onNextTransitionEnd, false);
+			visibleCallback();
 		}
 
 
