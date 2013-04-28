9594b9f74e488d6fbe403a81fa010a7ff47a0223
sole <listas@soledadpenades.com>
2013-04-28 08:41:54 +0100

    work on making request before showing map…

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -199,24 +199,24 @@ var PlanMyTripApp = function() {
 	}
 
 	function showMap() {
-		// TODO get trip landmarks from server, then show map
 
+		// TODO get trip landmarks from server, then show map
 		ajax(domain + 'trip/' + tripId + '.json')
 			.then(function() {
 
-			// First map with all landmarks
-			// On click show landmark + restaurant + cafe
-			showPage('map');
+				// First map with all landmarks
+				// On click show landmark + restaurant + cafe
+				showPage('map');
 
-			// TODO calculate average lat/lng and use to center map
-			var mapOptions = {
-				zoom: 8,
-				center: new google.maps.LatLng(-34.397, 150.644),
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			},
-			map = new google.maps.Map(itineraryMap, mapOptions);
+				// TODO calculate average lat/lng and use to center map
+				var mapOptions = {
+					zoom: 8,
+					center: new google.maps.LatLng(-34.397, 150.644),
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				},
+				map = new google.maps.Map(itineraryMap, mapOptions);
 
-		});
+			});
 
 	}
 
